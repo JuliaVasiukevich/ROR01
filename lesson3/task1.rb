e7e32d9f3f3894d5a2e7c6fd064403b781fafbd6
Julia Vasiukevich <julia.vasiukevich@profitero.com>
2023-01-23 22:22:21 +0400

    fix(lesson3): fix bug

--- lesson3/task1.rb
+++ lesson3/task1.rb
@@ -4,7 +4,7 @@
 # 4. обновляем количество попыток
 
 MIN = 0
-MAX = 100
+MAX = 10
 MAX_TRIES = 7
 INPUT_EQUEL = 'Вы выиграли!'
 INPUT_LOWER = 'Ваше число меньше загаданного!'
@@ -24,17 +24,20 @@ secret_number = rand(MIN..MAX)
 current_guess = (MAX + MIN) / 2
 min_number = MIN
 max_number = MAX
+tries = 0
 
-loop do
+while tries < MAX_TRIES do
+  p secret_number
+  tries += 1
   guess_result = right_guess?(current_guess, secret_number)
   
   if guess_result == INPUT_EQUEL
-    puts 'Победа!'
+    puts "Победа! Попытка #{tries}. Число #{secret_number}"
     break
   elsif guess_result == INPUT_LOWER
-    min_number = current_guess
+    min_number = current_guess + 1
   elsif guess_result == INPUT_HIGER
-    max_number = current_guess
+    max_number = current_guess - 1
   end
   current_guess = (min_number + max_number) / 2
 end
