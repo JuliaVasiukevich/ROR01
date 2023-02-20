55999f1bac469e957033590e6e20794645de78c5
Julia Vasiukevich <julia.vasiukevich@profitero.com>
2023-02-20 22:50:12 +0400

    init(lesson16): init joke task

---
+++ lesson16.rb
@@ -0,0 +1,48 @@
+# 1. взять api https://api.chucknorris.io/
+# 2. надо написать программу, которая будет позволять пользователю выбирать из доступных категорий и будет возвращать пользователю случайную шутку из этой категории
+
+require 'json'
+require 'curb'
+
+class JokesGenerator
+
+  HOST = 'https://api.chucknorris.io'
+  CATEGORY_PATH = '/jokes/categories'
+  JOKE_BY_CATEGORY = '/jokes/random?category='
+
+  def initialize
+    @categories = nil
+  end
+
+  class << self
+    def category_request
+      categories_request = Curl.get(HOST + CATEGORY_PATH)
+      @categories = JSON.parse(categories_request.body)
+
+      @categories.map.with_index do |element, index|
+        p "#{index + 1}: #{element}"
+      end
+    end
+
+    def joke_request(number_of_category)
+      raise StandardError 'Unknown value!' unless (1..@categories.size).include? number_of_category
+
+      chosen_category = @categories[number_of_category]
+      p "Your category is #{chosen_category}"
+      joke_request = Curl.get(HOST + JOKE_BY_CATEGORY + chosen_category)
+      p JSON.parse(joke_request.body)['value']
+    end
+  end
+end
+
+loop do
+  JokesGenerator.category_request
+
+  p 'Chose number of category'
+  number_of_category = gets.strip.to_i - 1
+
+  JokesGenerator.joke_request(number_of_category)
+
+  p 'press any button to continue...'
+  gets
+end
