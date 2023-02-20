094ac07d321a53c33e092d499c777f8993ccc83c
Julia Vasiukevich <julia.vasiukevich@profitero.com>
2023-02-20 21:02:19 +0400

    fix(lesson10,12): fix after review

--- lesson10/task1.rb
+++ lesson10/task1.rb
@@ -1,13 +1,17 @@
-# 1) Написать программу, которая проверяет что подаётся на ввод. Если подаётся что-то вместо числа - вызывать исключение и в обработке вывести, что было подано.
+# frozen_string_literal: true
 
-def is_number?
+# 1) Написать программу, которая проверяет что подаётся на ввод.
+# Если подаётся что-то вместо числа - вызывать исключение и в обработке вывести, что было подано.
+
+def number?
   p 'enter string that contain string or number'
   str = gets.strip
   begin
-    true if Float(str)
+    raise ArgumentError unless str.match(/^-?\d*([.,]\d+)?$/)
+    true
   rescue ArgumentError
     "False. It's not a number #{str}"
   end
 end
 
-p is_number?
+p number?
